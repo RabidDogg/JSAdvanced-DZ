0048a2a623dc166fc96bf1f5b705240b56930243
RabidDogg <platosh2012@yandex.ru>
2026-03-20 21:22:15 +0300

    12.12. Домашнее задание - Promises

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /**
- Сделайте запрос на https://pokeapi.co/api/v2/pokemon/ditto
- 
- После получения, получите информацию о первой его ablility по ссылке, которая приходит при первом запросе. Там найдите описание на английском и выведите в консоль:
+Задание:
+Изучив принцип работы с промисами, ваша задача — улучшить код из предыдущего домашнего задания.
+В предыдущем задании вы получали информацию о покемоне, затем получали данные о его первом умении (нулевая обилика) и, наконец, по ссылке из этой информации извлекали описание умения на английском языке.
+В отличие от предыдущего подхода, где использовались колбэки, сейчас необходимо реализовать этот же функционал, используя цепочки промисов.
+Обратите особое внимание на обработку возможных ошибок, например, если сервер не ответит. Улучшенный подход должен предусматривать корректную обработку исключительных ситуаций.
  */
 'use strict';
 
@@ -11,36 +13,59 @@ const methods = {
 	GET: 'GET',
 };
 
-function safeParse(jsonString) {
-	try {
-		return JSON.parse(jsonString);
-	} catch (error) {
-		console.error('Ошибка парсинга JSON:', error);
-		return null;
+// function safeParse(jsonString) {
+// 	try {
+// 		return JSON.parse(jsonString);
+// 	} catch (error) {
+// 		console.error('Ошибка парсинга JSON:', error);
+// 		return null;
+// 	}
+// }
+
+class FetchHelper {
+	static sendRequest(url, method = methods.GET) {
+		return fetch(url, {
+			method,
+		})
+			.then((resp) => {
+				if (!resp.ok) {
+					throw new Error(
+						`Ошибка при выполнении запроса: ${resp.status}`,
+					);
+				}
+				return resp.json();
+			})
+			.catch((error) => {
+				console.error(
+					`Возникло исключение при выполнении запроса: ${error}`,
+				);
+				throw error; // Пробрасываем ошибку дальше
+			});
 	}
 }
 
-class RequestHelper {
-	static sendRequest(method, url, callback) {
-		const xhr = new XMLHttpRequest();
-		xhr.open(method, url);
+// Старая реализация
+// class RequestHelper {
+// 	static sendRequest(method, url, callback) {
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.open(method, url);
 
-		xhr.addEventListener('load', function () {
-			if (this.status === 200) {
-				const data = safeParse(this.responseText);
-				callback(null, data);
-			} else {
-				callback(`Ошибка ${this.status}: ${this.statusText}`, null);
-			}
-		});
+// 		xhr.addEventListener('load', function () {
+// 			if (this.status === 200) {
+// 				const data = safeParse(this.responseText);
+// 				callback(null, data);
+// 			} else {
+// 				callback(`Ошибка ${this.status}: ${this.statusText}`, null);
+// 			}
+// 		});
 
-		xhr.addEventListener('error', function () {
-			callback('Сетевая ошибка', null);
-		});
+// 		xhr.addEventListener('error', function () {
+// 			callback('Сетевая ошибка', null);
+// 		});
 
-		xhr.send();
-	}
-}
+// 		xhr.send();
+// 	}
+// }
 
 class AbilityElement {
 	constructor(abilityData) {
@@ -49,17 +74,25 @@ class AbilityElement {
 		this.effect_entries = null;
 	}
 
-	getAbilityInfo(callback) {
-		RequestHelper.sendRequest(methods.GET, this.url, (err, data) => {
-			if (err) {
-				console.error('Ошибка получения способности:', err);
-				callback(err, null);
-				return;
-			}
+	fetchAbilityInfo() {
+		return FetchHelper.sendRequest(this.url).then((data) => {
 			this.effect_entries = data.effect_entries || [];
-			callback(null, this);
+			return this;
 		});
 	}
+
+	// Старая реализация
+	// getAbilityInfo(callback) {
+	// 	RequestHelper.sendRequest(methods.GET, this.url, (err, data) => {
+	// 		if (err) {
+	// 			console.error('Ошибка получения способности:', err);
+	// 			callback(err, null);
+	// 			return;
+	// 		}
+	// 		this.effect_entries = data.effect_entries || [];
+	// 		callback(null, this);
+	// 	});
+	// }
 
 	getEffect(language = 'en') {
 		if (!this.effect_entries) return null;
@@ -70,21 +103,38 @@ class AbilityElement {
 	}
 }
 
-// Использование
-RequestHelper.sendRequest(methods.GET, baseUrl, (err, pokemonData) => {
-	if (err) {
-		console.error('Ошибка загрузки покемона:', err);
-		return;
-	}
+FetchHelper.sendRequest(baseUrl)
+	.then((pokemonData) => {
+		const abilities = pokemonData.abilities;
+		if (!abilities || abilities.length === 0) {
+			throw new Error('Способности не найдены');
+		}
+		const ability = new AbilityElement(abilities[0]);
+		return ability.fetchAbilityInfo();
+	})
+	.then((ability) => {
+		const effect = ability.getEffect();
+		console.log('Описание способности:', effect);
+	})
+	.catch((error) => {
+		console.error(`Возникло исключение при выполнении задачи: ${error}`);
+	});
 
-	if (pokemonData.abilities && pokemonData.abilities.length > 0) {
-		const ability = new AbilityElement(pokemonData.abilities[0]);
+// Старая реализация
+// RequestHelper.sendRequest(methods.GET, baseUrl, (err, pokemonData) => {
+// 	if (err) {
+// 		console.error('Ошибка загрузки покемона:', err);
+// 		return;
+// 	}
 
-		ability.getAbilityInfo((err, abilityWithInfo) => {
-			if (err) return;
+// 	if (pokemonData.abilities && pokemonData.abilities.length > 0) {
+// 		const ability = new AbilityElement(pokemonData.abilities[0]);
 
-			const effect = abilityWithInfo.getEffect('en');
-			console.log('Описание способности:', effect);
-		});
-	}
-});
+// 		ability.getAbilityInfo((err, abilityWithInfo) => {
+// 			if (err) return;
+
+// 			const effect = abilityWithInfo.getEffect('en');
+// 			console.log('Описание способности:', effect);
+// 		});
+// 	}
+// });
